61d9837c1dbfbf2b163e2a7db17fede49564c7dc
Nick Brown <n.brown.professional@gmail.com>
2018-03-29 11:33:49 -0600

    Adds file structure and necessary files/keys

---
+++ liri.js
@@ -0,0 +1,14 @@
+
+// Reads and sets any environment variables 
+require("dotenv").config();
+
+var spotify = new Spotify(keys.spotify);
+var client = new Twitter(keys.twitter);
+
+// `my-tweets`
+
+// `spotify-this-song`
+
+// `movie-this`
+
+// `do-what-it-says`
